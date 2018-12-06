e12f430b0839af08a9a5bdeef05af2206671f61d
Reis Haleem <reishaleem@gmail.com>
2018-12-06 18:38:55 -0500

    refactor page titles into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return the page title for a page
+  def full_title(page_title = '')
+    base_title = 'OSU Book Exchange'
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
