4fa548b4180ec7ae084a5cce5771b6fa5db124f5
Reis Haleem <reishaleem@gmail.com>
2018-12-09 00:16:25 -0500

    start user login

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help' 
 
   get '/signup', to: 'users#new'
-
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
   resources :users
 end
