d4a37a12999f528909f1fae7ea17e562d272d28e
Reis Haleem <reishaleem@gmail.com>
2018-12-08 23:48:20 -0500

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+
+      if @user.save
+        flash[:success] = 'Welcome to the Book Exchange!'
+        redirect_to @user
+      else
+        render 'new'
+      end
+  end
+
+
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :phone, :rank, :age, :password, :password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   VALID_OSU_EMAIL_REGEX = /\A[a-zA-Z]+\.[1-9]\d*@osu\.edu/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_OSU_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   VALID_PHONE_REGEX = /\A\d{10}\z/
-  validates :phone, length: { maximum: 10 }, format: { with: VALID_PHONE_REGEX }
-  validates :age, :numericality => { :greater_than => 0, :less_than_or_equal_to => 100 }
+  validates :phone, length: { maximum: 10 }, format: { with: VALID_PHONE_REGEX }, :allow_nil => true
+  validates :age, :numericality => { :greater_than => 0, :less_than_or_equal_to => 100 }, :allow_nil => true
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
