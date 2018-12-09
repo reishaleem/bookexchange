a89bc638cc31e0eab888cc2fe26cd233c9e4b139
Reis Haleem <reishaleem@gmail.com>
2018-12-09 16:24:09 -0500

    finish basic user login

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help' 
 
   get '/signup', to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  post '/signup',  to: 'users#create'
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
   resources :users
 end
