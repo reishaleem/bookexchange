a296edb1ce16fe35c2a745601196fe5d4abbe539
Reis Haleem <reishaleem@gmail.com>
2018-12-13 18:21:52 -0500

    add pagination and only edit if user logged in to their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = @user.books.paginate(page: params[:page], per_page: 6)
   end
 
   def create
