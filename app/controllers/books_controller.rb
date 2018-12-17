53ab3ae96ffb73244478d1858a364ca764e42936
Reis Haleem <reishaleem@gmail.com>
2018-12-17 17:32:24 -0500

    only let logged in users access new book form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,17 @@
 class BooksController < ApplicationController
+  before_action :logged_in_user, only: [:new]
+
   def new
     @book = Book.new
   end
+
+  private
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
